3d6df3ad2ad7855bb3c23cf6cd3f83a7e1f804ae
sss <satbir.singh@planetinnovation.com.au>
2019-08-09 16:52:52 +1000

    added logic to label objects

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print ("welcome to kt")
+from image_reader import read
+from gui import gui
+from ip.find_mask import find_masks
+import cv2
+
+if __name__ == '__main__':
+    gui = gui.Gui()
+    input_image = 'a.jpeg'  # input("please input path to image")
+    img = read(input_image)
+    gui.add(img, "Original")
+    mask = find_masks(img, gui.add)
+    gui.add(mask)
+    overlay = cv2.add(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB), mask)
+    gui.add(overlay)
+
+    gui.show()
