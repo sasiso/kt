67fa7c0d7d1c9a07795fd82bcb6fffa00dac2ef0
satbirsoni <satbirsoni@gmail.com>
2019-08-10 17:02:53 +1000

    Labeling working now, ROI working now

--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@ from image_reader import read
 from gui import gui
 from ip.find_mask import find_masks
 import cv2
+import numpy as np
+
+show_ui = True
 
 if __name__ == '__main__':
     gui = gui.Gui()
     input_image = 'a.jpeg'  # input("please input path to image")
     img = read(input_image)
     gui.add(img, "Original")
-    mask = find_masks(img, gui.add)
+    ret, mask = find_masks(img, gui.add)
     gui.add(mask)
-    overlay = cv2.add(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB), mask)
-    gui.add(overlay)
 
-    gui.show()
+    gui.add(mask)
+    cv2.imwrite('mask.jpeg', mask)
+
+    if show_ui:
+        gui.show()
